ea243293e1d6f7ff0f6247676adaf3271c378c4c
eibrahimarisoy <eibrahimarisoy@gmail.com>
2022-05-24 23:24:00 +0300

    organise user and passenger admin

--- trip_api/core/admin.py
+++ trip_api/core/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
+from core.models import User
+
+
+class CustomUserAdmin(UserAdmin):
+    """
+    ined a custom admin model here to ensure that users created 
+    admin panel have their passwords hashed. For this, you need to 
+     'UserAdmin' instead of ModelAdmin.
+    """
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        (_('Personal info'), {'fields': ('status', 'first_name', 'last_name')}),
+        (_('Permissions'), {
+            'fields': (
+                'is_active', 'is_admin', 'is_staff',
+                'is_superuser', 'groups', 'user_permissions',
+            ),
+        }),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined', 'updated_at')}),
+        (_('Authentication Info'), {'fields': ('email', 'phone_number',)}),
+    )
+    readonly_fields = ['username', 'updated_at']
+    list_display = ('email', 'first_name', 'last_name', 'is_staff')
+    list_filter = ('is_staff', 'is_superuser', 'is_active', 'groups',)
+    search_fields = ('username', 'first_name', 'last_name', 'email')
+    ordering = ('username',)
+    filter_horizontal = ('groups', 'user_permissions',)
+
+admin.site.register(User, CustomUserAdmin)
--- trip_api/passengers/admin.py
+++ trip_api/passengers/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from passengers.models import Passenger
+
 # Register your models here.
+
+class CustomPassengerAdmin(admin.ModelAdmin):
+    search_fields = ('user__first_name', 'user__last_name', 'user__email')
+
+admin.site.register(Passenger, CustomPassengerAdmin)
