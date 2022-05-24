b5ff14065beabf4d078a131b205164e85e544082
eibrahimarisoy <eibrahimarisoy@gmail.com>
2022-05-24 23:09:02 +0300

    passenger model added

--- trip_api/passengers/models.py
+++ trip_api/passengers/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 
+class Passenger(models.Model):
+    user = models.OneToOneField(
+        'core.User',
+        on_delete=models.CASCADE,
+        primary_key=True,
+    )
+
+    class Meta:
+        verbose_name = "Passenger"
+        verbose_name_plural = "Passengers"
+
+    def __str__(self):
+        return f"{self.user.get_full_name()}"
