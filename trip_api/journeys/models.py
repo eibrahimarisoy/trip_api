285a3fc3512fe1e9c4bd93965c4fe1c689659b96
eibrahimarisoy <eibrahimarisoy@gmail.com>
2022-05-24 23:17:39 +0300

    added journey model

--- trip_api/journeys/models.py
+++ trip_api/journeys/models.py
@@ -1,16 +1,52 @@
 from django.contrib.gis.db import models
 from django.contrib.gis.geos import Point
+
+from core.models import BaseModel
 # Create your models here.
 
-class Journey(models.Model):
+class Journey(BaseModel):
     """
     Model for a journey
     """
-    start_location = models.PointField(
-        geography=True,
-        default=Point(0.0, 0.0)
+    passenger = models.ForeignKey(
+        'passengers.Passenger',
+        on_delete=models.CASCADE,
+        related_name='journeys',
     )
-    end_location = models.PointField(
-        geography=True,
-        default=Point(0.0, 0.0),
+    origin = models.PointField(
+        srid=4326,
+        null=True,
+        blank=True,
     )
+    destination = models.PointField(
+        srid=4326,
+        null=True,
+        blank=True,
+    )
+    distance = models.FloatField(
+        null=True,
+        blank=True,
+    )
+    duration = models.FloatField(
+        null=True,
+        blank=True,
+    )
+    price = models.FloatField(
+        null=True,
+        blank=True,
+    )
+    start_time = models.DateTimeField(
+        null=True,
+        blank=True,
+    )
+    end_time = models.DateTimeField(
+        null=True,
+        blank=True,
+    )
+
+    class Meta:
+        verbose_name = "Journey"
+        verbose_name_plural = "Journeys"
+
+    def __str__(self):
+        return f"{self.passenger.user.get_full_name()} - {self.start_time}"
