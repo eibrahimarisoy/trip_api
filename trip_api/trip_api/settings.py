dc2e18cdc06e8e9b0a1a1f592a48dcc61187f08e
eibrahimarisoy <eibrahimarisoy@gmail.com>
2022-05-25 01:14:19 +0300

    added drf simple-jwt for authentication

--- trip_api/trip_api/settings.py
+++ trip_api/trip_api/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'django.contrib.gis',
     'rest_framework_gis',
+    'rest_framework_simplejwt',
     
     "core",
     'passengers',
@@ -142,8 +143,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 REST_FRAMEWORK = {
-    # 'DEFAULT_AUTHENTICATION_CLASSES': (
-    # ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
     ],
--- trip_api/trip_api/urls.py
+++ trip_api/trip_api/urls.py
@@ -17,12 +17,16 @@ from django.contrib import admin
 from django.urls import path
 from passengers.views import PassengerViewset
 from rest_framework import routers
+from rest_framework_simplejwt.views import (TokenObtainPairView,
+                                            TokenRefreshView)
 
 router = routers.DefaultRouter()
 router.register(r'api/passengers', PassengerViewset)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
 
 urlpatterns += router.urls
